c93eabbde2324ae406fef768b6ddeebde65cbcc2
Quinn Slack <qslack@qslack.com>
2019-02-21 23:11:04 -0500

    Remove in-product /help, redirect to docs.sourcegraph.com
    - Revert e5e1c2393e4c9665f14da3eea3ad667a30ebdb05
    - Remove dependency on github.com/sourcegraph/docsite/markdown, revert to using previously used Markdown packages
      - Fix #2369
      - Help with #1361
    - Remove in-product rendered /help pages, redirect to equivalent https://docs.sourcegraph.com page for the current product version instead
      - Close #2337
      - Fix #1720
      - Fix #1912
      - Close #1885
      - Remove dependency on github.com/sourcegraph/docsite

--- cmd/frontend/graphqlbackend/file.go
+++ cmd/frontend/graphqlbackend/file.go
@@ -8,9 +8,9 @@ import (
 	"time"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/pkg/markdown"
 	"github.com/sourcegraph/sourcegraph/pkg/api"
 	"github.com/sourcegraph/sourcegraph/pkg/highlight"
-	"github.com/sourcegraph/sourcegraph/pkg/markdown"
 	"github.com/sourcegraph/sourcegraph/pkg/vcs/git"
 )
 
@@ -42,7 +42,7 @@ func (r *gitTreeEntryResolver) RichHTML(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return markdown.Render(content, nil)
+	return markdown.Render(content, nil), nil
 }
 
 type markdownOptions struct {
@@ -52,7 +52,7 @@ type markdownOptions struct {
 func (*schemaResolver) RenderMarkdown(args *struct {
 	Markdown string
 	Options  *markdownOptions
-}) (string, error) {
+}) string {
 	return markdown.Render(args.Markdown, nil)
 }
 
